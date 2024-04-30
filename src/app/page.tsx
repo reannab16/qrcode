fc34cb35e539cb4755488df090d0ff5a1c345bd0
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2024-04-30 15:51:19 +0100

    Fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,37 +2,51 @@
 
 import { Button } from "@mui/material";
 import { useSearchParams, useRouter } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Home() {
-  const searchParams = useSearchParams();
-  const user = searchParams.get("user");
+  function Search() {
+    const searchParams = useSearchParams();
+    const user = searchParams.get("user");
+
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        {user ? (
+          <div>Hello {user}</div>
+        ) : (
+          <div className="flex items-center justify-center flex-col w-full gap-y-2">
+            <div className="">What do you want to do today?</div>
+            <div className="flex items-center justify-center w-full gap-x-2">
+              <Button
+                variant="contained"
+                className=""
+                onClick={() => {
+                  router.push("/generate");
+                }}
+              >
+                Generate QR
+              </Button>
+              <Button
+                variant="contained"
+                className="text-nowrap"
+                onClick={() => {
+                  router.push("/scanner");
+                }}
+              >
+                Scan QR
+              </Button>
+            </div>{" "}
+          </div>
+        )}
+      </main>
+    );
+  }
+
   const router = useRouter();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      {user ? (
-        <div>Hello {user}</div>
-      ) : (
-        <div className="flex items-center justify-center flex-col w-full gap-y-2">
-          <div className="">What do you want to do today?</div>
-          <div className="flex items-center justify-center w-full gap-x-2">
-            <Button
-              variant="contained"
-              className=""
-              onClick={() => {
-                router.push("/generate")
-              }}
-            >Generate QR</Button>
-            <Button
-              variant="contained"
-              className="text-nowrap"
-              onClick={() => {
-                router.push("/scanner")
-              }}
-            >Scan QR</Button>
-          </div>{" "}
-        </div>
-      )}
-    </main>
+    <Suspense>
+      <Search />
+    </Suspense>
   );
 }
--- src/app/scanner/page.tsx
+++ src/app/scanner/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { useCallback, useEffect, useRef, useState } from "react";
-
-// Styles
-import "../styles/QrStyles.css";
+import { Suspense } from "react";
 
 // Qr Scanner
 import QrScanner from "qr-scanner";
@@ -19,107 +17,111 @@ export default function Scanner() {
   const [scannedResult, setScannedResult] = useState<string | undefined>("");
   const [startScan, setStartScan] = useState(false);
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams.toString())
-      params.set(name, value)
- 
-      return params.toString()
-    },
-    [searchParams]
-  )
+  function Search() {
+    const searchParams = useSearchParams();
+    const createQueryString = useCallback(
+      (name: string, value: string) => {
+        const params = new URLSearchParams(searchParams.toString());
+        params.set(name, value);
 
-  // Success
-  const onScanSuccess = (result: QrScanner.ScanResult) => {
-    console.log(result);
-    setScannedResult(result?.data);
-    router.push(pathname.replace("scanner","") + '?' + createQueryString('user', `${result?.data}`) );
-  };
+        return params.toString();
+      },
+      [searchParams]
+    );
 
-  // Fail
-  const onScanFail = (err: string | Error) => {
-    console.log(err);
-  };
-
-  useEffect(() => {
-    if (videoEl?.current && !scanner.current) {
-      // 👉 Instantiate the QR Scanner
-      scanner.current = new QrScanner(videoEl?.current, onScanSuccess, {
-        onDecodeError: onScanFail,
-        // 📷 This is the camera facing mode. In mobile devices, "environment" means back camera and "user" means front camera.
-        preferredCamera: "environment",
-        // 🖼 This will help us position our "QrFrame.svg" so that user can only scan when qr code is put in between our QrFrame.svg.
-        highlightScanRegion: true,
-        // 🔥 This will produce a yellow (default color) outline around the qr code that we scan, showing a proof that our qr-scanner is scanning that qr code.
-        highlightCodeOutline: true,
-        // 📦 A custom div which will pair with "highlightScanRegion" option above 👆. This gives us full control over our scan region.
-        overlay: qrBoxEl?.current || undefined,
-      });
-
-      // 🚀 Start QR Scanner
-      scanner?.current
-        ?.start()
-        .then(() => setQrOn(true))
-        .catch((err) => {
-          if (err) setQrOn(false);
-        });
-    }
-
-    // 🧹 Clean up on unmount.
-    // 🚨 This removes the QR Scanner from rendering and using camera when it is closed or removed from the UI.
-    return () => {
-      if (!videoEl?.current) {
-        scanner?.current?.stop();
-      }
-    };
-  }, [startScan]);
-
-  // ❌ If "camera" is not allowed in browser permissions, show an alert.
-  useEffect(() => {
-    if (!qrOn)
-      alert(
-        "Camera is blocked or not accessible. Please allow camera in your browser permissions and Reload."
+    // Success
+    const onScanSuccess = (result: QrScanner.ScanResult) => {
+      setScannedResult(result?.data);
+      router.push(
+        pathname.replace("scanner", "") +
+          "?" +
+          createQueryString("user", `${result?.data}`)
       );
-  }, [qrOn]);
+    };
+
+    // Fail
+    const onScanFail = (err: string | Error) => {
+      console.log(err);
+    };
+
+    useEffect(() => {
+      if (videoEl?.current && !scanner.current) {
+        // 👉 Instantiate the QR Scanner
+        scanner.current = new QrScanner(videoEl?.current, onScanSuccess, {
+          onDecodeError: onScanFail,
+          // 📷 This is the camera facing mode. In mobile devices, "environment" means back camera and "user" means front camera.
+          preferredCamera: "environment",
+          // 🖼 This will help us position our "QrFrame.svg" so that user can only scan when qr code is put in between our QrFrame.svg.
+          highlightScanRegion: true,
+          // 🔥 This will produce a yellow (default color) outline around the qr code that we scan, showing a proof that our qr-scanner is scanning that qr code.
+          highlightCodeOutline: true,
+          // 📦 A custom div which will pair with "highlightScanRegion" option above 👆. This gives us full control over our scan region.
+          overlay: qrBoxEl?.current || undefined,
+        });
+
+        // 🚀 Start QR Scanner
+        scanner?.current
+          ?.start()
+          .then(() => setQrOn(true))
+          .catch((err) => {
+            if (err) setQrOn(false);
+          });
+      }
+    }, [startScan]);
+
+    // ❌ If "camera" is not allowed in browser permissions, show an alert.
+    useEffect(() => {
+      if (!qrOn)
+        alert(
+          "Camera is blocked or not accessible. Please allow camera in your browser permissions and Reload."
+        );
+    }, [qrOn]);
+
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+          className=" border-[1px] border-slate-400 rounded-md shadow-sm flex items-center justify-center p-4 flex-col gap-y-2"
+        >
+          <div className="flex flex-col items-center justify-start p-2">
+            <div>i am allowing u to scan ur qr code</div>
+            <div className="text-sm italic font-light "></div>
+          </div>
+
+          <Button
+            variant="contained"
+            className="w-full"
+            onClick={() => {
+              setStartScan(true);
+            }}
+          >
+            scan qr code
+          </Button>
+
+          {startScan && (
+            <div className="w-40 h-40">
+              {/* QR */}
+              <video
+                ref={videoEl}
+                className="w-full h-full object-cover"
+              ></video>
+              <div ref={qrBoxEl} className="w-full left-0"></div>
+            </div>
+          )}
+        </Box>
+      </main>
+    );
+  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-        className=" border-[1px] border-slate-400 rounded-md shadow-sm flex items-center justify-center p-4 flex-col gap-y-2"
-      >
-        <div className="flex flex-col items-center justify-start p-2">
-          <div>i am allowing u to scan ur qr code</div>
-          <div className="text-sm italic font-light ">
-            
-          </div>
-        </div>
-
-        <Button
-          variant="contained"
-          className="w-full"
-          onClick={() => {
-            setStartScan(true);
-          }}
-        >
-          scan qr code
-        </Button>
-
-        {startScan && (
-          <div className="w-40 h-40">
-            {/* QR */}
-            <video ref={videoEl} className="w-full h-full object-cover"></video>
-            <div ref={qrBoxEl} className="w-full left-0"></div>
-          </div>
-        )}
-      </Box>
-    </main>
+    <Suspense>
+      <Search />
+    </Suspense>
   );
 }
